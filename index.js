d3aacbcdda56ab50b955886af45353d9bafb55a2
Matthew Larson <theguy@gurumojo.net>
2019-07-06 10:49:44 -0700

    fleshed out bitrise.io configs

--- index.js
+++ index.js
@@ -4,6 +4,6 @@
 
 import { AppRegistry } from 'react-native'
 import App from './lib/App'
-import { name as appName } from './app.json'
+import { name as appName } from './lib/app.json'
 
 AppRegistry.registerComponent(appName, () => App)
